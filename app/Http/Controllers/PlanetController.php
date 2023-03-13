ce55a2e378cae9de926e766a828d2690f3db2bfc
Emanuel Cruz <emanuelcruz091@gmail.com>
2023-03-13 19:43:27 -0300

    feat: add routes & controllers to person, planets and vehicles

---
+++ app/Http/Controllers/PlanetController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlanetController extends Controller
+{
+    public function all()
+    {
+        return 'all';
+    }
+
+    public function index($id)
+    {
+        return 'index';
+    }
+}
