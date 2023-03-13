ef9178b3892f8b3bb5d70430e2576a253f3df18c
Emanuel Cruz <emanuelcruz091@gmail.com>
2023-03-13 19:56:43 -0300

    feat: add endpoint for planets

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -3,16 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PlanetController extends Controller
 {
     public function all()
     {
-        return 'all';
+        $url = env('API_ENDPOINT');
+        $response = Http::get($url . '/planets');
+        $data = $response->json('results');
+
+        return response()->json([
+            'data' => $data,
+        ]);
     }
 
     public function index($id)
     {
-        return 'index';
+        $url = env('API_ENDPOINT');
+        $response = Http::get($url . '/planets/' . $id );
+        $data = $response->json();
+
+        return response()->json([
+            'data' => $data,
+        ]);
     }
 }
