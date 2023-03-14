d86e208f1f2e17ef3452f93c3ade8bb2fb675364
Emanuel Cruz <emanuelcruz091@gmail.com>
2023-03-13 21:00:00 -0300

    feat: add middleware to use in all endpoints

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -3,19 +3,35 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
+
     public function handle(Request $request, Closure $next)
     {
+        try {
+            JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+
+            if($e instanceof TokenInvalidException){
+                return response()->json([
+                     "status" => "token invalido"
+                ]);
+            }
+            if($e instanceof TokenExpiredException){
+                return response()->json([
+                     "status" => "El token expiro"
+                ]);
+            }
+
+            return response()->json(["status" => "El token no se encontró"]);
+        }
+
         return $next($request);
     }
 }
